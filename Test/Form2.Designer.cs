079e59b273e1b441ba890662eb4103036799b768
AntonioMV <li_antoniomv@unca.edu.mx>
2023-10-25 16:38:34 -0600

    cambios perrrs

--- Test/Form2.Designer.cs
+++ Test/Form2.Designer.cs
@@ -30,32 +30,46 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(368, 79);
+            pictureBox1.Location = new Point(80, 63);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(158, 239);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(354, 38);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(252, 333);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
             // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
             Name = "Form2";
             Text = "Form2";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
     }
 }
